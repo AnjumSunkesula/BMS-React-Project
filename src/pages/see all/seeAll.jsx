18b5075512426cd8c24b9060c161c0210d25864c
afroz <afrozdd7@gmail.com>
2024-09-07 21:37:35 +0530

    added movies cards and filter functionality and its css

--- src/pages/see all/seeAll.jsx
+++ src/pages/see all/seeAll.jsx
@@ -2,14 +2,53 @@ import './seeAll.css'
 import Header from '../../components/header/header';
 import React, { useState } from 'react';
 import { IoIosArrowDown, IoIosArrowUp } from "react-icons/io";
-import { BrowserRouter as Router, Switch, Route, useHistory  } from 'react-router-dom';
-import RecommendedMovies from '../../components/Recommended Movies/RM';
-import AboutMovie from '../../components/aboutmovie/AboutMovie'
-import MovieDetails from '../../components/aboutmovie/movieDetails';
+import {  useHistory  } from 'react-router-dom';
+import img1    from '../../assets/recommended movies/movie-cards/D & W.avif'
+import img2    from '../../assets/recommended movies/movie-cards/IEWU.avif'
+import img3    from '../../assets/recommended movies/movie-cards/double ismart.avif'
+import img4    from '../../assets/recommended movies/movie-cards/stree 2.avif'
+import img5    from '../../assets/recommended movies/movie-cards/inside out 2.avif'
+import img6    from '../../assets/recommended movies/movie-cards/DM4.avif'
+import img7    from '../../assets/recommended movies/movie-cards/gharat ganpati.avif'
+import img8    from '../../assets/recommended movies/movie-cards/KKM.avif'
+import img9    from '../../assets/recommended movies/movie-cards/nunakkuzhi.avif'
+import img10   from '../../assets/recommended movies/movie-cards/vedaa.avif'
+import img11   from  '../../assets/recommended movies/movie-cards/beetlejuice.avif'
+import img12   from  '../../assets/recommended movies/movie-cards/detective.avif'
+import img13   from  '../../assets/recommended movies/movie-cards/GOAT.avif'
+import img14   from  '../../assets/recommended movies/movie-cards/joker.avif'
+import img15   from  '../../assets/recommended movies/movie-cards/kill.avif'
+import img16   from  '../../assets/recommended movies/movie-cards/S-Sanivaram.avif'
+import img17   from  '../../assets/recommended movies/movie-cards/udaan choo.avif'
+import img18   from  '../../assets/recommended movies/movie-cards/alien.avif'
+import img19   from  '../../assets/recommended movies/movie-cards/gujarathi.avif'
+import img20   from '../../assets/recommended movies/movie-cards/marathi.avif'
+import img21   from  '../../assets/recommended movies/movie-cards/wedding.avif'
+import img22   from  '../../assets/recommended movies/movie-cards/vaazhai.avif'
+import img23   from  '../../assets/recommended movies/movie-cards/maharaja.avif'
+import img24   from  '../../assets/recommended movies/movie-cards/colony-2.avif'
+import img25   from  '../../assets/recommended movies/movie-cards/gangs-1.avif'
+import img26   from  '../../assets/recommended movies/movie-cards/strange darling.avif'
+import img27   from  '../../assets/recommended movies/movie-cards/RHTDM.avif'
+import img28   from  '../../assets/recommended movies/movie-cards/gangs.avif'
+import img29   from  '../../assets/recommended movies/movie-cards/17.avif'
+import img30   from '../../assets/recommended movies/movie-cards/laila majnu.avif'
+import img31   from  '../../assets/recommended movies/movie-cards/DDLJ.avif'
+
 
 function SeeAll () {
 
-    
+    const history = useHistory();
+    const startIndex = 0;
+
+    const handleMovieClick = (movie, index,) => { 
+        history.push({
+            pathname: '/aboutmovies',
+            state: {movie, index} 
+        });
+    };
+
+
 
     
 
@@ -63,6 +102,49 @@ function SeeAll () {
         }
     };
 
+    const movies = [
+    { img: img1  ,  movieName: 'deadpool & wolverine',       genre: 'action, adventure, comedy',           languages: 'english, hindi, tamil, telugu' },
+    { img: img2  ,  movieName: 'it ends with us',            genre: 'drama, romantic',                     languages: 'english' },
+    { img: img3  ,  movieName: 'double ismart',              genre: 'action, sci-fi, thriller',            languages: 'telugu, kannada, malayalam, tamil, hindi' },
+    { img: img4  ,  movieName: 'stree 2: sarkate ka aatank', genre: 'comedy, horror',                      languages: 'hindi' },
+    { img: img5  ,  movieName: 'inside out 2',               genre: 'animation, comedy, drama',            languages: 'english, hindi, tamil' },
+    { img: img6  ,  movieName: 'despicable me 4',            genre: 'action, adventure, animation, comedy',languages: 'english, hindi, tamil, telugu' },
+    { img: img7  ,  movieName: 'gharat ganpati',             genre: 'comedy, drama, family',               languages: 'marathi' },
+    { img: img8  ,  movieName: 'khel khel mein',             genre: 'comedy, drama',                       languages: 'hindi' },
+    { img: img9  ,  movieName: 'nunakkuzhi',                 genre: 'comedy, thriller',                    languages: 'malayalam' },
+    { img: img10 ,  movieName: 'vedaa',                      genre: 'action, drama',                       languages: 'hindi, tamil, telugu' },
+    { img: img11 ,  movieName: 'beetlejuice beetlejuice' ,   genre: 'comedy, fantasy, horror',             languages: 'english' },
+    { img: img12 ,  movieName: 'detective conan vs. kid the phantom thief', genre: 'anime, crime, mystery',languages: 'japanese' },
+    { img: img13 ,  movieName: 'the greatest of all time' ,  genre: 'action, drama, thriller',             languages: 'tamil, telugu' },
+    { img: img14 ,  movieName: 'joker' ,                     genre: 'drama, suspense, thriller',           languages: 'english' },
+    { img: img15 ,  movieName: 'kill' ,                      genre: 'action, thriller',                    languages: 'hindi' },
+    { img: img16 ,  movieName: 'saripodha sanivaram' ,       genre: 'action, drama',                       languages: 'telugu' },
+    { img: img17 ,  movieName: 'udan chhoo' ,                genre: 'comedy, drama, family',               languages: 'gujarathi' },
+    { img: img18 ,  movieName: 'alien: romulus' ,            genre: 'horror, sci-fi, thriller',            languages: 'english, hindi, telugu, tamil'},
+    { img: img19 ,  movieName: 'fakt purusho maate' ,        genre: 'comedy, drama, fantasy',              languages: 'gujarathi'},
+    { img: img20 ,  movieName: 'shriyut non maharashtrian' , genre: 'drama, mystery, thriller',            languages: 'marathi'},
+    { img: img21 ,  movieName: 'a wedding story' ,           genre: 'horror, thriller',                    languages: 'hindi'},
+    { img: img22 ,  movieName: 'vaazhai' ,                   genre: 'drama',                               languages: 'tamil'},
+    { img: img23 ,  movieName: 'maharaja (hindi)' ,          genre: 'action, drama',                       languages: 'hindi'},
+    { img: img24 ,  movieName: 'demonte colony 2-vengeance of the unholy' ,    genre: 'horror, thriller',  languages: 'tamil'},
+    { img: img25 ,  movieName: 'gangs of wasseypur' ,        genre: 'crime, drama, thriller',              languages: 'hindi'},
+    { img: img26 ,  movieName: 'strange darling' ,           genre: 'adventure, horror, mystery, thriller',languages: 'english'},
+    { img: img27 ,  movieName: 'rehnaa hai tere dil mein' ,  genre: 'drama, romantic',                     languages: 'hindi'},
+    { img: img28 ,  movieName: 'gangs of wasseypur 2' ,      genre: 'action, drama, thriller',             languages: 'hindi'},
+    { img: img29 ,  movieName: `seventeen tour 'follow' again to cinemas`, genre: 'documentary, musical',  languages: 'korean'},
+    { img: img30 ,  movieName: 'laila majnu',                genre: 'drama, romantic',                    languages: 'hindi'},
+    { img: img31 ,  movieName: 'dilwale dulhaniya le jayenge',genre: 'classic, drama, romantic',           languages: 'hindi'}
+    ];
+
+    //filter the movies based on selected filters
+    const filteredMovies = movies.filter((movie) => {
+        const languageMatch = selectedLanguages.length === 0 || selectedLanguages.some((lang) => movie.languages.includes(lang));
+        const genreMatch = selectedGenres.length === 0 || selectedGenres.some((genre) => movie.genre.includes(genre));
+        // const formatMatch = selectedFormatslength === 0 || selectedFormats.some((format) => movie.format.includes(format));
+
+        return languageMatch && genreMatch;
+
+    })
 
 
     return(
@@ -160,7 +242,7 @@ function SeeAll () {
                 </div>
             </div>
 
-          {/* movies container */}
+          {/*  container */}
 
             <div className='movie-container'>
                 <div className='selected-heading'>
@@ -210,6 +292,71 @@ function SeeAll () {
                         )}
                     </div>
                 </div>
+
+
+
+                <div className='movie-cards-container'>
+                    {filteredMovies.length > 0 ? (filteredMovies.map((movie, index) => (
+                       <div className='movie' key={index} >
+                            <div className='image'>
+                                <img 
+                                    src={movie.img} 
+                                    className='displayed-movie-cards'  
+                                    alt={movie.movieName}
+                                    onClick={() => handleMovieClick(movie, index)}
+                                />
+                            </div>
+                            <div className='dVLjtu'>
+                                <div className='dshUgd'>
+                                    <div className='film-title'>{movie.movieName}</div>
+                                </div>
+                                <div className='dshUgd'>
+                                    <div className='Ylsgya'>{movie.genre}</div>
+                                </div>
+                                <div className='dshUgd'>
+                                    <div className='Ylsgya'>{movie.languages}</div>
+                                </div>
+                            </div>
+                        </div> 
+                        ))
+                    ) : (
+                          <p>No movies found for the selected filters.</p>
+                    )}
+
+
+
+
+                    {/* {movies.slice(startIndex, startIndex + 31).map((movie, index) => (
+                        <div className='movie' key={index} >
+                        <div className='image'>
+                            <img 
+                                src={movie.img} 
+                                className='displayed-movie-cards'  
+                                alt={movie.movieName}
+                                onClick={() => handleMovieClick(movie, index)}
+                            />
+                        </div>
+                        <div className='dVLjtu'>
+                            <div className='dshUgd'>
+                                <div className='film-title'>{movie.movieName}</div>
+                            </div>
+                            <div className='dshUgd'>
+                                <div className='Ylsgya'>{movie.genre}</div>
+                            </div>
+                            <div className='dshUgd'>
+                                <div className='Ylsgya'>{movie.languages}</div>
+                            </div>
+                        </div>
+                    </div>
+                    ))} */}
+                    
+                </div>
+
+                
+                
+
+
+
             </div>
 
         </div>
