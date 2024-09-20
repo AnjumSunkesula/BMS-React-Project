0aeb9e8eb2b3ad23f14b505dbafda9c22efb9d0d
afroz <afrozdd7@gmail.com>
2024-09-20 19:30:25 +0530

    chnaged route paths for home,AboutMovies and BookTickets

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,43 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { BrowserRouter as Router, Switch, Route  } from 'react-router-dom';
 import Login from '../src/pages/login';
 import Home from '../src/pages/home';
 import AboutMovies from './components/aboutmovie/AboutMovie';
 import SeeAll from './pages/see all/seeAll'
 import BookTickets from './pages/ticket booking/ticketBooking';
+
 function App() {
+
+  const [selectedCity, setSelectedCity] = useState(   // Retrieve selectedCity from localStorage, or default to 'Select City'
+    () => localStorage.getItem('selectedCity') || 'select city'
+  );                         //when u go to another page the selected city should be displayed instead of'select city'.thats why using useEffect and useState
+
+  useEffect(() => {        //Using useEffect to store the selectedCity in localStorage whenever it changes
+    localStorage.setItem('selectedCity', selectedCity);
+  }, [selectedCity]);
+
+
+
   return(
     <>
     <Router>
       <Switch>
         <Route exact path="/" component={Login}/>
-        <Route exact path="/home" component={Home}/>
-        <Route path="/aboutmovies" component={AboutMovies}/>
+
+        <Route exact path="/home"> 
+          <Home selectedCity={selectedCity} setSelectedCity={setSelectedCity} /> {/* Pass selectedCity and setSelectedCity to Home */}
+        </Route>
+
+        <Route  path="/aboutmovies"> 
+          <AboutMovies selectedCity={selectedCity} setSelectedCity={setSelectedCity} /> {/* Pass selectedCity and setSelectedCity to AboutMovies */}
+        </Route>
+
         <Route path= "/seeall" component={SeeAll} />
-        <Route path="/booktickets" component={BookTickets}/>
+
+        <Route path="/booktickets">
+          <BookTickets selectedCity={selectedCity} /> {/* Pass selectedCity to BookTickets */}
+        </Route>
+
       </Switch>
     </Router>
     </>
@@ -25,26 +48,3 @@ export default App;
 
 
 
-// import React from 'react';
-// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-// import Login from './pages/login';
-// import Home from './pages/home';
-// import RegionSelection from './components/citySelection/regionselection';
-// import ViewAllCities from './components/citySelection/viewallcities';
-// import AboutMovies from './components/aboutmovie/AboutMovie';
-
-// function App() {
-//   return (
-//     <Router>
-//       <Routes>
-//         <Route path="/" element={<Login />} />
-//         <Route path="/home" element={<Home />} />
-//         <Route path="/regionselection" element={<RegionSelection />} />
-//         <Route path="/viewallcities" element={<ViewAllCities />} />
-//         <Route path="/aboutmovies" element={<AboutMovies />} />
-//       </Routes>
-//     </Router>
-//   );
-// }
-
-// export default App;
